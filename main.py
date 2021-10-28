74acf7796d8193eb1ec698009813acdafcef664f
Damian Tabor <zettie008@gmail.com>
2021-10-28 16:20:58 +0100

    Add slot for opening file dialog

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+import random
+
+from PySide2 import QtCore, QtWidgets, QtGui
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Eartquakes information")
+
+        # Menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
+
+        # Open QAction
+        open_action = QtWidgets.QAction("Open", self)
+        open_action.setShortcut(QtGui.QKeySequence.Quit)
+        open_action.triggered.connect(self.open_file_dialog)
+
+        # Exit QAction
+        exit_action = QtWidgets.QAction("Exit", self)
+        exit_action.setShortcut(QtGui.QKeySequence.Quit)
+        exit_action.triggered.connect(self.close)
+
+        self.file_menu.addAction(open_action)
+        self.file_menu.addAction(exit_action)
+
+        # Status Bar
+        self.status = self.statusBar()
+        self.status.showMessage("Get byte array")
+
+        # Window dimensions
+        geometry = qApp.desktop().availableGeometry(self)
+        self.setFixedSize(geometry.width() * 0.5, geometry.height() * 0.5)
+
+        textbox = QtWidgets.QWidget
+
+        self.setCentralWidget(QtWidgets.QTextEdit())
+
+    @QtCore.Slot()
+    def open_file_dialog(self):
+        options = QtWidgets.QFileDialog.Options()
+        fileName = QtWidgets.QFileDialog.getOpenFileName(self,
+            "Open Image", ".", "Image Files (*.png *.jpg *.jpeg *.bmp)")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    test = MainWindow()
+    test.show()
+    sys.exit(app.exec_())
