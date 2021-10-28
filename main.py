7e9bf58186d766f2e42ec41dd5414b363fdd2b24
Damian Tabor <zettie008@gmail.com>
2021-10-28 16:28:08 +0100

    Refactor w/ 'blue'

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ from PySide2 import QtCore, QtWidgets, QtGui
 
 
 class MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Eartquakes information")
+        self.setWindowTitle('Eartquakes information')
 
         # Menu
         self.menu = self.menuBar()
-        self.file_menu = self.menu.addMenu("File")
+        self.file_menu = self.menu.addMenu('File')
 
         # Open QAction
-        open_action = QtWidgets.QAction("Open", self)
+        open_action = QtWidgets.QAction('Open', self)
         open_action.setShortcut(QtGui.QKeySequence.Quit)
         open_action.triggered.connect(self.open_file_dialog)
 
         # Exit QAction
-        exit_action = QtWidgets.QAction("Exit", self)
+        exit_action = QtWidgets.QAction('Exit', self)
         exit_action.setShortcut(QtGui.QKeySequence.Quit)
         exit_action.triggered.connect(self.close)
 
@@ -29,7 +28,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Status Bar
         self.status = self.statusBar()
-        self.status.showMessage("Get byte array")
+        self.status.showMessage('Get byte array')
 
         # Window dimensions
         geometry = qApp.desktop().availableGeometry(self)
@@ -41,8 +40,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
     @QtCore.Slot()
     def open_file_dialog(self):
-        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self,
-            "Open Image", ".", "Image Files (*.png *.jpg *.jpeg *.bmp)")
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Open Image', '.', 'Image Files (*.png *.jpg *.jpeg *.bmp)'
+        )
 
 
 if __name__ == '__main__':
